e09e855d66b6d3a06ee46ec8609ed565c4c27318
Lynn Cheng <lynn811112@gmail.com>
2022-07-07 22:15:24 +0800

    增加評論圖片審核功能

--- src/main/resources/static/js/comment.js
+++ src/main/resources/static/js/comment.js
@@ -10,6 +10,22 @@ function saveComment(e) {
 }
 
 jQuery(document).ready(function ($) {
+	
+    // const settings = {
+    //     "async": true,
+    //     "crossDomain": true,
+    //     "url": "https://car-data.p.rapidapi.com/cars/years",
+    //     "method": "GET",
+    //     "headers": {
+    //         "X-RapidAPI-Key": "05d771cdbemshe143263788b0713p19cda0jsn7c44fa1258b1",
+    //         "X-RapidAPI-Host": "car-data.p.rapidapi.com"
+    //     }
+    // };
+
+    // $.ajax(settings).done(function (response) {
+    //     console.log(response);
+    // });
+	
     // 立即顯示多張照片 - 評論
     $('#imagefiles').on('change', function () {
         var imagefiles = document.getElementById("imagefiles");
@@ -18,7 +34,9 @@ jQuery(document).ready(function ($) {
         for (i = 0; i < number; i++) {
             var urls = URL.createObjectURL(event.target.files[i]);
             document.getElementById("formFile").innerHTML +=
-                '<div class="col-sm-4 mb-3"> <img class="rounded mb-3" src="' + urls + '" /> </div>';
+                '<div class="col-12 mb-3"> <img class="rounded mb-3" src="' + urls + '" /> </div>';
+            processImageFile(event.target.files[i]);
+			
         }
     })
 
@@ -34,9 +52,58 @@ jQuery(document).ready(function ($) {
         }
     })
 
+    function processImageFile(imageObject) {
+            //確認區域與所選擇的相同，因為使用免費的，所以區域選West Center US
+        var uriBase = "https://computer-vision-lynn.cognitiveservices.azure.com/vision/v2.1/analyze";
+        
+        var params = {
+            "visualFeatures": "Adult",
+            "details": "",
+            "language": "en",
+        };
+        //顯示分析的圖片
+		let result = true;
+        //送出分析
+        $.ajax({
+            url: uriBase + "?" + $.param(params),
+            // Request header
+            beforeSend: function(xhrObj){
+                xhrObj.setRequestHeader("Content-Type","application/octet-stream");
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "6f158e623cca4015bc077bf379c7186d");
+            },
+            type: "POST",
+            processData:false,
+            contentType:false,
+            // Request body
+            data: imageObject
+        })
+        .done(function(data) {
+            //顯示JSON內容
+            
+            if (data.adult.isAdultContent == true || data.adult.isRacyContent == true ||  data.adult.isGoryContent == true) {
+                console.log('請勿上傳色情、暴力或任何包含成人內容的圖片')
+                $('#isAdult').css('display', 'block');
+            } else {
+            	hideInvalidText($('#formFile'));
+            	$('#isAdult').css('display', 'none');
+			}
+
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            //丟出錯誤訊息
+//            var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+//            errorString += (jqXHR.responseText === "") ? "" : jQuery.parseJSON(jqXHR.responseText).message;
+//            alert(errorString);
+        });
+    };
+
+
+
+
+    let contentMax = 200;
+    let imagefilesMax = 3;
+
     $(function () {
-        let contentMax = 200;
-        let imagefilesMax = 3;
 
         let contentLength = $("#content").val().length;
         $("#content-length").text('(' + contentLength + '/' + contentMax + ')');
@@ -175,7 +242,7 @@ jQuery(document).ready(function ($) {
                 $("td").on('click', function () {
                     let id = $(this).closest("tr").find('td').eq(1).html();
                     $("#itemId").val(id);
-                    $("#itemTb").val("tickets")
+                    $("#itemTb").val(itemTb)
                     // form validation 設定更改
                     if ($("form").hasClass('validated')) {
                         $('#itemTb').val() !== null ? hideInvalidText($('#itemTb')) : showInvalidText($('#itemTb'));
@@ -210,14 +277,25 @@ jQuery(document).ready(function ($) {
             success: function (data) {
                 let itemInfo;
                 if (itemTb == "tickets") {
-                    itemInfo = "<table><tbody><tr><td>商品類型：</td><td>景點票券</td></tr>" +
+                    itemInfo = "<table class='text-left'><colgroup><col span='1' style='width: 20%;'><col span='1' style='width: 80%;'>" + 
+                    	"<tbody><tr><td>商品項目：</td><td>景點票券</td></tr>" +
+                        "<tr><td>商品類型：</td><td>" + data.tagName +"</td></tr>" +
                         "<tr><td>商品編號：</td><td>" + data.ticketNo +"</td></tr>" +
                         "<tr><td>商品名稱：</td><td>" + data.ticketName +"</td></tr>" +
                         "<tr><td>商品價格：</td><td>NT$" + data.price +"</td></tr>" +
                         "<tr><td>電話：　　</td><td>" + data.phone +"</td></tr>" +
-                        "<tr><td>地址：　　</td><td>" + data.city + data.location + data.address+"</td></tr></tbody></table>" ;
-                } else if (itemTb == "tickets") {
-					
+                        "<tr><td>地址：　　</td><td>" + data.city + data.location + data.address+"</td></tr>" +
+                        "<tr><td>商品簡述：　　</td><td>" + data.ticketIntro +"</td></tr></tbody></table>" ;
+                } else if (itemTb == "hotels") {
+					 itemInfo = "<table class='text-left'>" + 
+                    	"<tbody><tr><td>商品項目：</td><td>飯店住宿</td></tr>" +
+                        "<tr><td>商品類型：</td><td>" + data.hotelStyle +"</td></tr>" +
+                        "<tr><td>商品編號：</td><td>" + data.id +"</td></tr>" +
+                        "<tr><td>商品名稱：</td><td>" + data.hotel_name +"</td></tr>" +
+                        "<tr><td>商品價格：</td><td>NT$" + data.price +"</td></tr>" +
+                        "<tr><td>電話：　　</td><td>" + data.phone +"</td></tr>" +
+                        "<tr><td>地址：　　</td><td>" + data.city + data.fullAddress +"</td></tr>" +
+                        "<tr><td>商品簡述：　　</td><td>飯店設備 - " + data.provide +"</td></tr></tbody></table>" ;
 				}
                 //                    else if (String(parsed.tableName) == "行程") {
                 //                        itemInfo = "商品項目：" + parsed.tableName +
@@ -230,6 +308,7 @@ jQuery(document).ready(function ($) {
                 //                    }
                 Swal.fire({
                     html: '<div class="text-dark text-start small">' + itemInfo + '</div>',
+                    // width: 800,
                     confirmButtonColor: '#FF8D29',
                 })
             },
@@ -242,3 +321,5 @@ jQuery(document).ready(function ($) {
 
 
 })
+
+
