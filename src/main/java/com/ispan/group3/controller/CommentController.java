00209f6b5ab53de3d91f5eacecb74cda9c52f82a
KennyShow <thoroughgoingkill@gmail.com>
2022-07-14 14:31:57 +0800

    hotelDetail 1 version

--- src/main/java/com/ispan/group3/controller/CommentController.java
+++ src/main/java/com/ispan/group3/controller/CommentController.java
@@ -1,85 +1,85 @@
-package com.ispan.group3.controller;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-
-import com.ispan.group3.repository.Comment;
-import com.ispan.group3.repository.CommentImage;
-import com.ispan.group3.service.CommentImageService;
-import com.ispan.group3.service.CommentService;
-import com.ispan.group3.util.FileUploadUtil;
-
-@Controller
-public class CommentController {
-
-	private final CommentService cService;
-	private final CommentImageService iService;
-
-	@Autowired
-	public CommentController(CommentService cService, CommentImageService iService) {
-		this.cService = cService;
-		this.iService = iService;
-	}
-
-	@GetMapping("/backend/comments")
-	public String findAll(Model model) {
-		List<Comment> comments = cService.findAll();
-		model.addAttribute("comments", comments);
-		return "backend/comment-list";
-	}
-
-	@GetMapping({"/backend/comments/form", "/backend/comments/form/{id}"})
-	public String showForm(Model model, @PathVariable(required = false) Integer id) {
-		Comment comment;
-		if (id != null) {
-			comment = cService.findById(id);
-		} else {
-			comment = new Comment();			
-		}
-		model.addAttribute("comment", comment);
-		return "backend/comment-form";
-	}
-
-	@PostMapping("/backend/comments")
-	public String save(@ModelAttribute Comment comment, 
-					   @RequestParam(value = "imagefiles", required = false) List<MultipartFile> files,
-					   @RequestParam(value = "deleteImages", required = false) List<Integer> deleteImages) {
-		List<CommentImage> images = new ArrayList<>();
-		for (MultipartFile file : files) {
-			try {
-				String savePath = FileUploadUtil.saveFile("comment", file);
-				CommentImage commentImage = new CommentImage(savePath, comment);
-				images.add(commentImage);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		comment.setImages(images);
-		cService.save(comment);
-		for (Integer deleteImage : deleteImages) {
-			iService.deleteById(deleteImage);
-		}
-
-		return "redirect:/backend/comments";
-	}
-
-	@DeleteMapping("/backend/comments/{id}")
-	public String deleteComment(@PathVariable Integer id) {
-		cService.deleteById(id);
-		return "redirect:/backend/comments";
-	}
-	
-
-}
+//package com.ispan.group3.controller;
+//
+//import java.io.IOException;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.DeleteMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import com.ispan.group3.repository.Comment;
+//import com.ispan.group3.repository.CommentImage;
+//import com.ispan.group3.service.CommentImageService;
+//import com.ispan.group3.service.CommentService;
+//import com.ispan.group3.util.FileUploadUtil;
+//
+//@Controller
+//public class CommentController {
+//
+//	private final CommentService cService;
+//	private final CommentImageService iService;
+//
+//	@Autowired
+//	public CommentController(CommentService cService, CommentImageService iService) {
+//		this.cService = cService;
+//		this.iService = iService;
+//	}
+//
+//	@GetMapping("/backend/comments")
+//	public String findAll(Model model) {
+//		List<Comment> comments = cService.findAll();
+//		model.addAttribute("comments", comments);
+//		return "backend/comment-list";
+//	}
+//
+//	@GetMapping({"/backend/comments/form", "/backend/comments/form/{id}"})
+//	public String showForm(Model model, @PathVariable(required = false) Integer id) {
+//		Comment comment;
+//		if (id != null) {
+//			comment = cService.findById(id);
+//		} else {
+//			comment = new Comment();
+//		}
+//		model.addAttribute("comment", comment);
+//		return "backend/comment-form";
+//	}
+//
+//	@PostMapping("/backend/comments")
+//	public String save(@ModelAttribute Comment comment,
+//					   @RequestParam(value = "imagefiles", required = false) List<MultipartFile> files,
+//					   @RequestParam(value = "deleteImages", required = false) List<Integer> deleteImages) {
+//		List<CommentImage> images = new ArrayList<>();
+//		for (MultipartFile file : files) {
+//			try {
+//				String savePath = FileUploadUtil.saveFile("comment", file);
+//				CommentImage commentImage = new CommentImage(savePath, comment);
+//				images.add(commentImage);
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//		}
+//		comment.setImages(images);
+//		cService.save(comment);
+//		for (Integer deleteImage : deleteImages) {
+//			iService.deleteById(deleteImage);
+//		}
+//
+//		return "redirect:/backend/comments";
+//	}
+//
+//	@DeleteMapping("/backend/comments/{id}")
+//	public String deleteComment(@PathVariable Integer id) {
+//		cService.deleteById(id);
+//		return "redirect:/backend/comments";
+//	}
+//
+//
+//}
--- src/main/java/com/ispan/group3/repository/Hotel.java
+++ src/main/java/com/ispan/group3/repository/Hotel.java
@@ -1,62 +1,58 @@
 package com.ispan.group3.repository;
 
 
-import java.sql.Blob;
-import java.sql.Timestamp;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Transient;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.persistence.*;
+import java.sql.Blob;
+import java.sql.Timestamp;
+
 @Getter
 @Setter
 @Entity
 @Table(name = "Hotel")
 @Component("Hotel")
 public class Hotel {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;  //自動生成
-	private String  status;  //是專業經理 Or 自由業者
-	private String  hotelStyle;   // 公寓 & 整間小屋  & 整棟住宅 & 別墅 & 專業大飯店
-	private Integer hotelArea;  // 接受使用者  住宿大小坪數
-	private Integer bathCount;  // 衛浴數量
-	private Integer bedroomCount;  //房間數量
-	private Integer bedCount;   //床的數量
-	private String  fullAddress;  //完整地址
-	private String  notional;  //國家
-	private String  city;   //城市
-	private String  hotelRule;  //住宿規則
-	private String  Howgoto;   //顧客該如何去
-	private String  hotel_name;  // 住宿的Name
-	private String  introduce;   //介紹你的hotel
-	private String  provide;    // checkbox 多值
-	private Integer phone;     //聯絡的電話
-	private Integer price;     ////一晚的價格
-	private String  boss_name;   //聯絡人名子
-	private String  roomtype;    //單人房  Or  雙人房 or Both
-	private Integer peopleCount;  //可住人數
-	@JsonIgnore
-	private  Blob image;   //之後接受照片的容器
-	private  String fileName; //接檔案名稱
-	@CreationTimestamp
-	private  Timestamp admissionTime; //街道檔案的那時候時間
-	@Transient
-	@JsonIgnore
-	private  MultipartFile productImage;//前端照片
-	
-	
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;  //自動生成
+    private String status;  //是專業經理 Or 自由業者
+    private String hotelStyle;   // 公寓 & 整間小屋  & 整棟住宅 & 別墅 & 專業大飯店
+    private Integer hotelArea;  // 接受使用者  住宿大小坪數
+    private Integer bathCount;  // 衛浴數量
+    private Integer bedroomCount;  //房間數量
+    private Integer bedCount;   //床的數量
+    private String fullAddress;  //完整地址
+    private String notional;  //國家
+    private String city;   //城市
+    private String hotelRule;  //住宿規則
+    private String Howgoto;   //顧客該如何去
+    private String hotel_name;  // 住宿的Name
+    private String introduce;   //介紹你的hotel
+    private String provide;    // checkbox 多值
+    private Integer phone;     //聯絡的電話
+    private Integer price;     ////一晚的價格
+    private String boss_name;   //聯絡人名子
+    private String roomtype;    //單人房  Or  雙人房 or Both
+    private Integer peopleCount;  //可住人數
+    private String feature1;// 特色1
+    private String feature2;// 特色1
+    private String feature3;// 特色1
+    @JsonIgnore
+    private Blob image;   //之後接受照片的容器
+    private String fileName; //接檔案名稱
+    @CreationTimestamp
+    private Timestamp admissionTime; //街道檔案的那時候時間
+    @Transient
+    @JsonIgnore
+    private MultipartFile productImage;//前端照片
+
+
 //	public Integer getId() {
 //		return id;
 //	}
@@ -201,8 +197,6 @@ public class Hotel {
 //	public void setProductImage(MultipartFile productImage) {
 //		this.productImage = productImage;
 //	}
-
-
 
 
 }
