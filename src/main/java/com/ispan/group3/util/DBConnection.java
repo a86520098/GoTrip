0ddb77e29cf4c518363c6b03685f32ae7fc5ddc4
KennyShow <thoroughgoingkill@gmail.com>
2022-07-15 11:06:16 +0800

    修正前端細節

--- src/main/java/com/ispan/group3/util/DBConnection.java
+++ src/main/java/com/ispan/group3/util/DBConnection.java
@@ -1,21 +1,21 @@
-package com.ispan.group3.util;
-import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
-
-import javax.sql.DataSource;
-
-public class DBConnection {
-
-    private static final String USER = "sa";
-    private static final String PASSWORD = "sa123456";
-
-    public static DataSource dataSource() {
-        SQLServerDataSource ds = new SQLServerDataSource();
-        ds.setServerName("localhost");
-        ds.setPortNumber(1433);
-        ds.setUser(USER);
-        ds.setPassword(PASSWORD);
-        return ds;
-    }
-
-	}
-
+//package com.ispan.group3.util;
+////import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+//
+//import javax.sql.DataSource;
+//
+//public class DBConnection {
+//
+//    private static final String USER = "sa";
+//    private static final String PASSWORD = "sa123456";
+//
+//    public static DataSource dataSource() {
+////        SQLServerDataSource ds = new SQLServerDataSource();
+//        ds.setServerName("localhost");
+//        ds.setPortNumber(1433);
+//        ds.setUser(USER);
+//        ds.setPassword(PASSWORD);
+//        return ds;
+//    }
+//
+//}
+//
