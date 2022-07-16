36a113f8ee8a08cfc9f9471b8e77c77f9e51b5dd
KennyShow <thoroughgoingkill@gmail.com>
2022-07-16 15:37:04 +0800

    圖片多張即時顯示

--- src/main/java/com/ispan/group3/util/DBConnection.java
+++ src/main/java/com/ispan/group3/util/DBConnection.java
@@ -1,22 +1,22 @@
-//package com.ispan.group3.util;
-//
-//import javax.sql.DataSource;
-//
-//import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
-//
-//public class DBConnection {
-//
-//    private static final String USER = "sa";
-//    private static final String PASSWORD = "sa123456";
-//
-//    public static DataSource dataSource() {
-//        SQLServerDataSource ds = new SQLServerDataSource();
-//        ds.setServerName("localhost");
-//        ds.setPortNumber(1433);
-//        ds.setUser(USER);
-//        ds.setPassword(PASSWORD);
-//        return ds;
-//    }
-//
-//}
-//
+package com.ispan.group3.util;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+
+import javax.sql.DataSource;
+
+public class DBConnection {
+
+    private static final String USER = "sa";
+    private static final String PASSWORD = "sa123456";
+
+    public static DataSource dataSource() {
+        SQLServerDataSource ds = new SQLServerDataSource();
+        ds.setServerName("localhost");
+        ds.setPortNumber(1433);
+        ds.setUser(USER);
+        ds.setPassword(PASSWORD);
+        return ds;
+    }
+
+}
+
