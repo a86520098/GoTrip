0f1717d5638b65f508edb1fa8b4421076e4b66df
KennyShow <thoroughgoingkill@gmail.com>
2022-07-20 12:12:58 +0800

    Hotel >> find   HotelRoom (multiple)   is OK!!

---
+++ src/main/java/com/ispan/group3/service/impl/HotelServiceimpl.java
@@ -0,0 +1,48 @@
+package com.ispan.group3.service.impl;
+
+import com.ispan.group3.repository.Hotel;
+import com.ispan.group3.repository.HotelRepositoryInterFace;
+import com.ispan.group3.service.HotelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Service
+@Transactional
+public class HotelServiceimpl implements HotelService {
+    @Autowired
+    HotelRepositoryInterFace hotelRepository;
+
+    @Override
+    public Object save(Hotel hol) {
+        return hotelRepository.save(hol);
+    }
+
+    @Override
+    public void update(Hotel hol) {
+    }
+
+    @Override
+    public List<Hotel> findAll() {
+        return hotelRepository.findAll();
+    }
+
+    @Override
+    public void delete(Integer id) {
+        hotelRepository.deleteById(id);
+    }
+
+    @Override
+    public Hotel findById(Integer id) {
+        return hotelRepository.findById(id).get();
+    }
+
+    @Override
+    public List<Hotel> findByCompany(Integer id) {
+        return hotelRepository.findByCompany(id);
+    }
+
+}
